17ca2aff44e1ea0ab247492dcad4b6756e845a31
Andrew Pinkham <code@andrewsforge.com>
2015-08-20 10:22:19 -0500

    Use sphinxcontrib-fulltoc.
    The sphinxcontrib-fulltoc project automatically adds a ToC (Table of
    Contents) to the sidebar of generated sphinx documentation.
    
        https://sphinxcontrib-fulltoc.readthedocs.org/en/latest/

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
